7b8d566b1fde20f9203cb90dbdd1c14dc0f7b8d3
thibeau003 <thibeau.raes@bazandpoort.be>
2022-09-23 14:47:07 +0200

    Made header fixed position

--- src/header.php
+++ src/header.php
@@ -3,7 +3,7 @@ require_once "tailwind.php";
 ?>
 
 <header>
-    <nav class="bg-slate-800 px-4 lg:px-6 py-2.5">
+    <nav class="fixed w-screen bg-slate-800 px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="./" class="flex items-center">
                 <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->

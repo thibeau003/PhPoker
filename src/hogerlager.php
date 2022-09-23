039e59d8dd6c22c1c6b56905cd807372bd014dc4
SysGerm <85944242+SysGerm@users.noreply.github.com>
2022-09-23 15:18:28 +0200

    Update hogerlager.php

--- src/hogerlager.php
+++ src/hogerlager.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hoger Lager</title>
 </head>
-<body>
+
+<body class="bg-slate-700" >
     <?php
-        include "./tailwind.php";
-        include "./header.php";
+    include "./tailwind.php";
+    include "./header.php";
     ?>
 </body>
+
 </html>

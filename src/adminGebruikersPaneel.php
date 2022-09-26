24beb61fec4dc92d109b25cb83dc208aa89de672
Jelbe2004 <113691108+Jelbe2004@users.noreply.github.com>
2022-09-26 15:03:23 +0200

    account verwijderen als admin

---
+++ src/adminGebruikersPaneel.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin users panel</title>
+</head>
+
+<body class="bg-slate-700">
+    <?php
+    include "./tailwind.php";
+    include "./header.php";
+    include "connect.php";
+    session_start();
+    $sql = "select * from users";
+    $resultaat = $mysqli->query($sql);
+    print "<table>";
+    while ($row = $resultaat->fetch_assoc()) {
+        print "<tr><td><a href= 'gebruikerverwijderen.php?gebruiker=" . $row['username'] . "'>" . $row['username'] . "</td></tr>";
+    }
+    print "</table>";
+    print "<a href=index.php> go back to the main page</a>";
+    ?>
+</body>
+
+</html>

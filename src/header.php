d653055fb5d656a0a66b8a4974007dcd200e824a
SysGerm <114411278+jitse003@users.noreply.github.com>
2022-09-30 15:59:36 +0200

    Header en Hoger Lager Styling + Knoppen

--- src/header.php
+++ src/header.php
@@ -1,18 +1,18 @@
 <?php
-    session_start();
+session_start();
 
-    require_once "tailwind.php";
-    
-    // $_SESSION['user'] = Array("username" => "Warre", "balance" => "1000");
+require_once "tailwind.php";
 
-    if(isset($_SESSION["user"])){
-        $user = $_SESSION["user"];
+// $_SESSION['user'] = Array("username" => "Warre", "balance" => "1000");
 
-        $username = $user['username'];
-        $first2Characters = substr($username, 0, 2);
-        $balance = $user['balance'];
+if (isset($_SESSION["user"])) {
+    $user = $_SESSION["user"];
 
-        echo "
+    $username = $user['username'];
+    $first2Characters = substr($username, 0, 2);
+    $balance = $user['balance'];
+
+    echo "
             <header>
                 <nav class='fixed w-screen bg-slate-800 px-4 lg:px-6 py-2.5'>
                     <div class='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
@@ -23,12 +23,12 @@
                         <div class='flex items-center lg:order-2'>
                             <div class='bg-slate-900 rounded-full mr-6'>
                                 <a href='./balance.php'>
-                                    <p class='text-left text-white pl-4 pr-12 py-2'>$". $balance ."</p>
+                                    <p class='text-left text-white pl-4 pr-12 py-2'>$" . $balance . "</p>
                                 </a>
                             </div>
 
                             <button type='button' onclick='handleAccountMenu()' class='aspect-square flex mr-3 rounded-full md:mr-0 bg-slate-900 text-white' id='user-menu-button' aria-expanded='false' data-dropdown-toggle='user-dropdown' data-dropdown-placement='bottom'>
-                                <span class='p-2'>". $first2Characters ."</span>
+                                <span class='p-2'>" . $first2Characters . "</span>
                             </button>
       
                             <div class='absolute w-auto top-14 ml-32 float-right'>
@@ -50,7 +50,7 @@
                         <div class='hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1' id='mobile-menu-2'>
                             <ul class='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0'>
                                 <li>
-                                    <a href='#' class='block py-2 pr-4 pl-3 text-white lg:border-0 lg:p-0'>Game</a>
+                                    <a href='./higherlower.php' class='block py-2 pr-4 pl-3 text-white lg:border-0 lg:p-0'>Higher Lower</a>
                                 </li>
                                 <li>
                                     <a href='#' class='block py-2 pr-4 pl-3 text-white lg:border-0 lg:p-0'>Game</a>
@@ -70,8 +70,8 @@
                 </nav>
             </header>
         ";
-    } else {
-        echo "
+} else {
+    echo "
             <header>
                 <nav class='fixed w-screen bg-slate-800 px-4 lg:px-6 py-2.5'>
                     <div class='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
@@ -115,11 +115,11 @@
                 </nav>
             </header>
         ";
-    }
+}
 ?>
 
 <script>
-    function handleAccountMenu(){
+    function handleAccountMenu() {
         document.getElementById('user-dropdown').classList.toggle("hidden")
     }
 </script>

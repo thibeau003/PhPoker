3ecfa7ffb0e17879d9649096090e77069cf4dc6c
Jelbe2004 <113691108+Jelbe2004@users.noreply.github.com>
2022-09-30 14:04:28 +0200

    added delete user link

--- src/adminUsersPanel.php
+++ src/adminUsersPanel.php
@@ -18,7 +18,7 @@
     $resultaat = $mysqli->query($sql);
     print "<table>";
     while ($row = $resultaat->fetch_assoc()) {
-        print "<tr><td><a href= 'deleteUser.php?gebruiker=" . $row['username'] . "'>" . $row['username'] . "</td></tr>";
+        print "<tr><td>" . $row['username'] . "</td><td><a href= 'deleteUser.php?gebruiker=" . $row['username'] . "'> delete user </tr>";
     }
     print "</table>";
     print "<a href='index.php'> go back to the main page</a>";

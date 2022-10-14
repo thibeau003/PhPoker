5e0412a3b34d5e0494729330f2283c0444bb5f3c
Jelbe2004 <113691108+Jelbe2004@users.noreply.github.com>
2022-10-14 14:22:12 +0200

    added (non functioning) buttons

--- src/mines.php
+++ src/mines.php
@@ -14,4 +14,92 @@
     include "./header.php";
     include "./connect.php";
     session_start();
+    echo "<div class='mt-10 w-2/3 mx-auto'>
+                                    <div class=''>
+                                        <div class='flex flex-row h-auto my-1'>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='21'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='22'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                        </div>
+                                        <div class='flex flex-row h-auto my-1'>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='11'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='12'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='13'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                        </div>
+                                        <div class='flex flex-row h-auto my-1'>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='11'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='12'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='13'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                        </div>
+                                        <div class='flex flex-row h-auto my-1'>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='11'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='12'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='13'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                        </div>
+                                        <div class='flex flex-row h-auto my-1'>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='11'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='12'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='13'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                            <button class='w-1/3 mx-1 rounded text-center text-white bg-slate-800'>
+                                                <p id='23'>$0.00</p>
+                                            </button>
+                                        </div>
+                                    </div>";
     ?>

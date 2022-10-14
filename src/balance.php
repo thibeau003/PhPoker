d2511f86a166496447e0c1096521ee36b03a482f
lowie <lowie.casteels@bazandpoort.be>
2022-10-14 15:56:48 +0200

    Withdraw money from account -- Lowie

--- src/balance.php
+++ src/balance.php
@@ -59,8 +59,57 @@
                     </script>"
                     ;
                     
-            } else {
+            } else if (isset($_POST['withdraw_amount'])) {
+
+                if ($_SESSION["user"]["balance"] < $_POST['withdraw_amount']) {
+                    Header("Location: ./balance.php");
+                    exit();
+                }
+
+                $sql = "UPDATE tblusers SET balance = (balance - " . $_POST['withdraw_amount'] * 100 . ") WHERE user_id = " . $_SESSION['user']['user_id'];
+                $resultaat = $mysqli->query($sql);
+                $_SESSION['user']['balance'] = $_SESSION['user']['balance'] - $_POST['withdraw_amount'] * 100; 
+
                 echo "
+                        <div class='py-8 mx-auto'>
+                            <svg xmlns='http://www.w3.org/2000/svg' class='w-1/2 mx-auto h-1/2' fill='green' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>
+                                <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
+                            </svg>
+                        </div>
+                    ";
+
+                    echo "
+                    <script>
+                        $.ajax(
+                        './updateUserSession.php?user_id=". $_SESSION['user']['user_id'] ."',
+                        {
+                            success: function(data) {
+                                document.getElementById('balanceText').textContent = '$' + ". $_SESSION['user']['balance'] ." / 100
+                            },
+                            error: function() {
+                                alert('There was some error performing the AJAX call!');
+                            }
+                        }
+                        );
+                    </script>"
+                    ;
+            } else {
+                if (isset($_GET["action"]) && $_GET["action"] == "withdraw") {
+                    echo "
+                    <p class='text-xl font-medium py-4'>Withdraw</p>
+        
+                    <form action='' method='post'>  
+                        <div class='w-1/2 mx-auto'>
+                            <label class='text-left w-1/2' for='amount'>Enter an amount</label>
+                            <br>
+                            <input class='text-center w-full bg-transparent border-b-2 mb-4 border-slate-700 focus:outline-none' type='amount' id='withdraw_amount' name='withdraw_amount' required>
+                        </div>
+                        
+                        <input type='submit' class='w-1/2 bg-slate-900 rounded-full mb-4' value='Withdraw'>
+                    </form>
+                ";
+                } else {
+                    echo "
                         <p class='text-xl font-medium py-4'>Deposit</p>
             
                         <form action='' method='post'>  
@@ -73,21 +122,10 @@
                             <input type='submit' class='w-1/2 bg-slate-900 rounded-full mb-4' value='Pay'>
                         </form>
                     ";
+                }
             }
             ?>
         </div>
-
-        <?php
-        if (isset($_GET["action"]) && $_GET["action"] == "deposit") {
-            echo "
-                
-                ";
-        } elseif (isset($_GET["action"]) && $_GET["action"] == "Withdraw") {
-            echo "
-                
-                ";
-        }
-        ?>
     </div>
 </body>
 

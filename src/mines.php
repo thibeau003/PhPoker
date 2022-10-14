f87ac61a24107987d127cba7d939d676b247cb0e
Derkkon <siebe.vanpuyenbroeck@bazandpoort.be>
2022-10-14 15:39:39 +0200

    improved buttons

--- src/mines.php
+++ src/mines.php
@@ -16,99 +16,117 @@
     session_start();
     echo "
     <div class='mt-10 w-2/3 mx-auto'>
-        <div class=''>
+        <div class='keuzeKnoppen'>
             <div class='flex flex-row  my-1'>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='21'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 p-7 rounded text-center text-white bg-slate-800'>
+                    <p id='1'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='22'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='2'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='3'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='4'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
-                </button>
-            </div>
-            <div class='flex flex-row h-auto my-1'>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='11'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='12'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='13'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='5'></p>
                 </button>
             </div>
             <div class='flex flex-row h-auto my-1'>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='11'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 p-7 rounded text-center text-white bg-slate-800'>
+                    <p id='6'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='12'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='7'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='13'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='8'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='9'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
-                </button>
-            </div>
-            <div class='flex flex-row h-auto my-1'>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='11'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='12'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='13'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
-                </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='10'></p>
                 </button>
             </div>
             <div class='flex flex-row h-auto my-1'>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='11'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 p-7 rounded text-center text-white bg-slate-800'>
+                    <p id='11'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='12'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='12'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='13'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='13'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='14'></p>
                 </button>
-                <button class='w-1/3 mx-1 mb-5 p-3 rounded text-center text-white bg-slate-800'>
-                    <p id='23'>$0.00</p>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='15'></p>
+                </button>
+            </div>
+            <div class='flex flex-row h-auto my-1'>
+                <button class='w-1/3 mx-1 mb-5 p-7 rounded text-center text-white bg-slate-800'>
+                    <p id='16'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='17'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='18'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='19'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='20'></p>
+                </button>
+            </div>
+            <div class='flex flex-row h-auto my-1 '>
+                <button class='w-1/3 mx-1 mb-5 p-7 rounded text-center text-white bg-slate-800'>
+                    <p id='21'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='22'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='23'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='24'></p>
+                </button>
+                <button class='w-1/3 mx-1 mb-5 rounded text-center text-white bg-slate-800'>
+                    <p id='25'></p>
                 </button>
             </div>
         </div>
-    </div>
-                                    
-    <div class='mt-6 '>
-        <form action='' method='POST'>
-            <input type='text' name='towersBettingAmount' placeholder='Enter an amount to bet.' class='w-full py-1 px-2 bg-transparent border border-slate-800 rounded focus:outline-none'>
-            <input type='submit' value='Start Game' class='w-full py-2 mt-2 bg-slate-800 rounded text-white'>
-        </form>
+        <div class='mt-6 '>
+            <form action='' method='POST'>
+                <input type='text' name='towersBettingAmount' placeholder='Enter an amount to bet.' class='w-full mb-3 py-1 px-2 bg-transparent border border-slate-800 rounded focus:outline-none'>
+                <p class =' mb-3 text-white text-center'>The amount of mines.</p>
+                <div class ='keuzeBomKnoppen'> 
+                    <div class='flex flex-row h-auto my-1'>
+                        <button class='w-1/3 mx-1 mb-3 p-3 rounded text-center text-white bg-slate-800'>
+                            <p id=''>3</p>
+                        </button>
+                        <button class='w-1/3 mx-1 mb-3 p-3 rounded text-center text-white bg-slate-800'>
+                            <p id=''>5</p>
+                        </button>
+                        <button class='w-1/3 mx-1 mb-3 p-3 rounded text-center text-white bg-slate-800'>
+                            <p id=''>10</p>
+                        </button>
+                        <button class='w-1/3 mx-1 mb-3 p-3 rounded text-center text-white bg-slate-800'>
+                            <p id=''>20</p>
+                        </button>
+                    </div>    
+                </div>
+                <input type='submit' value='Start Game' class='w-full py-2 mt-2 bg-slate-800 rounded text-white'>
+            </form>
+        </div>
     </div>";
+                                    
+    
     ?>

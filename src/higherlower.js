318a866bc5f1ec1800b56e945a1fb33d37260c0e
AntonDoms <anton.doms@bazandpoort.be>
2022-11-14 16:10:17 +0100

    blijven spelen

--- src/higherlower.js
+++ src/higherlower.js
@@ -1,10 +1,4 @@
 function changeOpacity(){
-    var counter = 0;
-
-    if(counter  % 2 == 0 || counter == 0) {
         document.getElementById("speelkaart").style.opacity = 100;
-    } else {
-        document.getElementById("speelkaart").style.opacity = 0;
-    }
-    counter++
+
 }
--- src/higherlower.php
+++ src/higherlower.php
@@ -13,18 +13,45 @@
     <?php
     include "./tailwind.php";
     include "./header.php";
-    include "./connect.php";
+    include "./connect.php";	
+    if ($_SESSION['bet']) {
+        echo
+        '<form method="post">
+        <button type="submit" name="back" class="pt-20 text-white float-left ml-12 text-xl hover:underline">&#x2190 Back</button>
+              </form>';
+    }
+
+    if (isset($_POST['back'])) {
+        unset($_SESSION['higherlower']['startgame']);
+    }
     ?>
-    <a class="pt-20 text-white float-left ml-12 text-xl" href="./higherlower.php">&#x2190 Back</a>
     <div class="pt-20 text-center text-white absolute inset-x-0 bottom-20">
         <div class="grid grid-cols-3 container mx-auto text-center mb-10">
             <?php
+            if (isset($_POST['higher'])) {
+                if ($_SESSION['higherlower']['deck'][$_SESSION['nextCard']] > $_SESSION['higherlower']['deck'][$_SESSION['currentCard']]) {
+                    echo "true";
+                } else {
+                    header("location: higherlower.php?lost");
+                }
+            }
+            if (isset($_POST['lower'])) {
+                if ($_SESSION['higherlower']['deck'][$_SESSION['nextCard']] < $_SESSION['higherlower']['deck'][$_SESSION['currentCard']]) {
+                    echo "TRUE";
+                } else {
+                    header("location: higherlower.php?lost");
+                }
+            }
+
+            if (isset($_POST['startgame'])) {
+                $_SESSION['higherlower']['startgame'] = true;
+            }
+
             if (!isset($_SESSION['higherlower']['startgame'])) {
                 $_SESSION["higherlower"]["deck"] = range(1, 52);
                 $_SESSION['higherlower']['startgame'] = false;
                 shuffle($_SESSION["higherlower"]["deck"]);
             }
-
 
             if (!isset($_SESSION['currentCard'])) {
                 $_SESSION["currentCard"] = 1;
@@ -34,57 +61,62 @@
                 $_SESSION['nextCard']++;
             }
 
-            if (!($_SESSION['higherlower']['startgame'])) {
-                echo '
-                <form action="higherlower.php" method="post">
-                <button name="startgame" type="submit" onclick="startGame()">Start Game</button>
-                </form>
-                ';
-                if (isset($_POST['startgame'])) {
-                    $_SESSION['higherlower']['startgame'] = true;
-                }
-            }
 
             if ($_SESSION['higherlower']['startgame']) {
-
                 echo '<img class="mx-auto" src="./assets/cards/' . $_SESSION['higherlower']['deck'][$_SESSION['currentCard']] . '.png" width="65%">';
-                echo '<img class="opacity-100 mx-auto" src="./assets/cards/' . $_SESSION['higherlower']['deck'][$_SESSION['nextCard']] . '.png" width="65%" id="speelkaart">';
-                echo '<img class="mx-auto" src="./assets/cards/A" width="65%">';
-
-                if (isset($_POST['higher']) or isset($_POST['lower'])) {
-                }
+                echo '<img class="opacity-0 mx-auto" src="./assets/cards/' . $_SESSION['higherlower']['deck'][$_SESSION['nextCard']] . '.png" width="65%" id="speelkaart">';
+                echo '<img class="mx-auto" src="./assets/cards/A.png" width="65%">';
             }
 
             ?>
 
         </div>
         <?php
+        if (!$_SESSION['higherlower']['startgame']) {
+            echo '<form action="" method="post">';
+            echo '<input class="bg-slate-800 rounded-lg border-white border-2 text-center" type="number" name="bet" max="' . $_SESSION['user']['balance'] . '"  min="1" required>';
+            echo "<br>";
+            echo "<br>";
+            echo '<button class="p-2 bg-slate-800 rounded" type="submit" name="knop">Bet this value</button>';
+            echo '</form>';
+        }
 
-        if ($_SESSION['higherlower']['startgame'] == true) {
-            print '<div>
-            <form action="" method="post" id="myform">
-            <button class="bg-slate-800 square-lg p-8 mb-5 rounded" type="submit" onclick="changeOpacity();" name="higher">Higher &#8593</button>
-            <button class="bg-slate-800 square-lg p-8 mb-5 rounded" type="submit" onclick="changeOpacity();" name="lower">Lower &#8595</button>
-            </form>
-            </div>';
-        } else {
-            if (isset($_POST['knop'])) {
-                $_SESSION['bet'] = $_POST['bet'];
-                $_SESSION['user']['balance'] -= $_SESSION['bet'] * 100;
-                if ($_SESSION['user']['balance'] < $_SESSION['bet']) {
-                    echo "error with betting";
-                } else {
-                    echo "<p>Bet: $" . $_SESSION['bet'] . ".00</p>";
+
+        if (!$_SESSION['higherlower']['startgame']) {
+            if (!$_SESSION['higherlower']['startgame']) {
+                if (isset($_POST['knop'])) {
+                    $_SESSION['bet'] = $_POST['bet'];
+                    $_SESSION['user']['balance'] -= $_SESSION['bet'] * 100;
+                    if ($_SESSION['user']['balance'] < $_SESSION['bet']) {
+                        echo "error with betting";
+                    } else {
+                        echo "<p>Bet: $" . $_SESSION['bet'] . ".00</p>";
+                    }
                 }
-            } else {
-                echo '<form action="" method="post">';
-                echo '<input class="bg-slate-800 rounded-lg border-white border-2 text-center" type="number" name="bet" max="' . $_SESSION['user']['balance'] . '"  min="1" required>';
-                echo "<br>";
-                echo "<br>";
-                echo '<button class="p-2 bg-slate-800 rounded" type="submit" name="knop">Bet this value</button>';
-                echo '</form>';
             }
         }
+
+        if ($_SESSION['higherlower']['startgame'] == false) {
+            echo '
+                    <form action="higherlower.php" method="post">
+                    <button name="startgame" class="mx-auto text-center p-5" type="submit" onclick="startGame()">Start Game</button>
+                    </form>
+                    ';
+        }
+        if ($_SESSION['higherlower']['startgame']) {
+            print '
+                <div>
+                <form action="" method="post">
+                    <button class="text-xl p-1 hover:underline text-center" type="submit">Next Card</button>
+                </form>
+                <button class="bg-slate-800 square-lg p-5 mb-5 rounded" type="submit" onclick="changeOpacity();" name="higher">Higher &#8593</button>
+                <button class="bg-slate-800 square-lg p-5 mb-5 rounded" type="submit" onclick="changeOpacity();" name="lower">Lower &#8595</button>
+
+                </div>
+                ' . "Bet: $" . $_SESSION['bet'] . ".00" . '
+                ';
+        }
+
         ?>
     </div>
 </body>

cd5ae3ee644ff75a22542519b8ebc18ac710cd90
SysGerm <114411278+jitse003@users.noreply.github.com>
2023-01-30 15:41:31 +0100

    aaaaaaa

--- src/higherlower.js
+++ src/higherlower.js
@@ -2,5 +2,6 @@ function changeOpacity(){
         document.getElementById("speelkaart").style.opacity = 100;
 }
 
-// gaaber met anton en jitse en warre en nog is anton
+// gaaber met anton en jitse en warre en nog is anton en jits   
+
 
